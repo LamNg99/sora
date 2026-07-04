b192ef7e1f82633443312c72f1a68025c5a29332
Lam Nguyen <tunglam030699@gmail.com>
2026-07-04 16:56:31 -0600

    configure settings

---
+++ packages/cli/src/components/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <box
+      justifyContent="center"
+      alignItems="center"
+    >
+        <box flexDirection="row" justifyContent="center" alignItems="center" gap={0.5}>
+          <ascii-font font="tiny" text="Sora" />
+        </box>
+    </box>
+  );
+}
